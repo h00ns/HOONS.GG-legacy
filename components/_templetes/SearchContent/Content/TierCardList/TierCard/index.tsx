76d96200a0833cbeb543b1e5ac7f106c4643c9b8
jihoon <jihoon.hwang@marketofmaterial.com>
2023-05-26 11:05:40 +0900

    [feat] tier, rank, win, lose

--- components/_templetes/SearchContent/Content/TierCardList/TierCard/index.tsx
+++ components/_templetes/SearchContent/Content/TierCardList/TierCard/index.tsx
@@ -8,6 +8,8 @@ import { getSummonerDetailDataType } from '@customType/summoner';
 import Typography from '@components/_atoms/Typography';
 import { TypoSize } from '@constants/atoms/Typography';
 import Image from 'next/image';
+import Divider from '@components/_atoms/Divider';
+import { gray } from '@styles/Colors';
 
 type Props = {
   title: string;
@@ -15,10 +17,13 @@ type Props = {
 };
 
 const Content = styled.div`
+  display: grid;
+  row-gap: 8px;
+`;
+
+const InfoWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  row-gap: 12px;
-  align-items: center;
+  column-gap: 24px;
 `;
 
 const TierImageWrapper = styled.div`
@@ -27,8 +32,18 @@ const TierImageWrapper = styled.div`
   position: relative;
 `;
 
+const DescriptionWrapper = styled.div`
+  flex: 1;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 4px;
+  justify-content: center;
+`;
+
 export default function TierCard({ title, data }: Props) {
   const { tier, rank, leaguePoints, wins, losses } = data ?? {};
+  const avg = Math.floor((wins! / (wins! + losses!)) * 100);
 
   const UN_RANK = 'UNRANK';
   const TierImageUrl = `/assets/images/tier/${tier ?? UN_RANK}.png`;
@@ -36,10 +51,34 @@ export default function TierCard({ title, data }: Props) {
   return (
     <Card>
       <Content>
-        <TierImageWrapper>
-          <Image src={TierImageUrl} layout="fill" alt="tier" />
-        </TierImageWrapper>
-        <Typography size={TypoSize.SH3}>{title}</Typography>
+        <Typography size={TypoSize.B4}>{title}</Typography>
+        <Divider />
+
+        <InfoWrapper>
+          <TierImageWrapper>
+            <Image src={TierImageUrl} layout="fill" alt="tier" />
+          </TierImageWrapper>
+          <DescriptionWrapper>
+            {tier ? (
+              <>
+                <Typography size={TypoSize.SH2}>
+                  {tier} {rank}
+                </Typography>
+                <Typography size={TypoSize.B3} color={gray.gray6}>
+                  {leaguePoints} LP
+                </Typography>
+                <Typography size={TypoSize.B5} color={gray.gray6}>
+                  {wins}승 {losses}패 승률 {avg}%
+                </Typography>
+              </>
+            ) : (
+              // only UNRANKED
+              <Typography size={TypoSize.B3} color={gray.gray6}>
+                UNRANKED
+              </Typography>
+            )}
+          </DescriptionWrapper>
+        </InfoWrapper>
       </Content>
     </Card>
   );

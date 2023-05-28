f0fd888b7d8796cac002e4995b38ae32cd03e179
jihoon <jihoon.hwang@marketofmaterial.com>
2023-05-28 13:01:40 +0900

    [feat] apiUrlCompiler

--- pages/api/match/[puuid].js
+++ pages/api/match/[puuid].js
@@ -4,7 +4,9 @@ import { apiRegionUrlCompiler } from "..";
 export default async function handler(request, response) {
   const { puuid } = request.query;
 
-  const res = await fetch(apiRegionUrlCompiler(`/lol/match/v5/matches/by-puuid/${puuid}/ids`));
+  const res = await fetch(apiRegionUrlCompiler(`/lol/match/v5/matches/by-puuid/${puuid}/ids`, {
+    count: 10
+  }));
   const data = await res.json();
   return response.status(res.status).json({ data });
 }

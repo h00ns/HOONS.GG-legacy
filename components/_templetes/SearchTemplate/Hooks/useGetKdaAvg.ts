30e06f65c749143c05fd24ecf393bd6ac90a61b8
jihoon <jihoon.hwang@marketofmaterial.com>
2023-05-29 14:36:47 +0900

    [fix] 오타 및 useGetKdaAvg 버그 수정

--- components/_templetes/SearchTemplate/Hooks/useGetKdaAvg.ts
+++ components/_templetes/SearchTemplate/Hooks/useGetKdaAvg.ts
@@ -1,5 +1,5 @@
 export const useGetKdaAvg = (kills?: number, deaths?: number, assists?:number): number => {
-  if (!kills || !deaths || !assists) return 0
+  if (kills === undefined || deaths === undefined || assists === undefined) return 0
 
   return Math.floor(((kills+assists) / deaths) * 100) / 100;
 }

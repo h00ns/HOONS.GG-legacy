c78889c1e2448dfed3f2d7c601b93a794d482513
jihoon <jihoon.hwang@marketofmaterial.com>
2023-05-29 12:54:36 +0900

    [feat] 다국어 처리

--- components/_templetes/SearchTemplate/Hooks/useGetGameTime.ts
+++ components/_templetes/SearchTemplate/Hooks/useGetGameTime.ts
@@ -1,17 +1,21 @@
+import { useTranslation } from "next-i18next";
+
 // 초 -> 시,분,초 변환
 export const useGetGameTime = (seconds: number) => {
+  const { t } = useTranslation('search');
+
   if (seconds < 60) {
-    return `${seconds}초`
+    return `${seconds}${t('sec')}`
   }
   else if (seconds < 3600) {
     const min = Math.floor(seconds / 60);
     const sec = seconds - min * 60;
-    return `${min}분 ${sec}초`
+    return `${min}${t('min')} ${sec}${t('sec')}`
   }
   else {
     const hours = Math.floor(seconds / 3600)
     const min = Math.floor((seconds - hours * 3600) / 60)
     const sec = seconds - hours * 3600 - min * 60
-    return `${hours}시 ${min}분 ${sec}초`
+    return `${hours}${t('hour')} ${min}${t('min')} ${sec}${t('sec')}`
   }
 }
